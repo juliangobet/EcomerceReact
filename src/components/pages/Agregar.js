aaa4afb5ed08c63569cc643720c1c891bb58de93
Julian Gobet <juliangobet38@gmail.com>
2022-02-22 19:29:07 -0300

    NavBar

---
+++ src/components/pages/Agregar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Agregar = () => {
+    return (
+        <div>
+            <h1>Esta es la pagina de agregar</h1>
+        </div>
+    )
+} 
+export default Agregar
---
+++ src/components/pages/Libros.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Libros = () => {
+    return (
+        <div>
+            <h1>Esta es la pagina libros</h1>
+        </div>
+    )
+} 
+export default Libros
